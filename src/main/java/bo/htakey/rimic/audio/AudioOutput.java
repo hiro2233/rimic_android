0bfebf4e804b9a92cc5275cb12418c2fc2217e33
Hiroshi Takey F. (hiro2233) <htakey@gmail.com>
2020-09-03 04:25:57 -0400

    audio: output: fixed locking audiotrack.

--- src/main/java/bo/htakey/rimic/audio/AudioOutput.java
+++ src/main/java/bo/htakey/rimic/audio/AudioOutput.java
@@ -25,7 +25,6 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Process;
-import android.provider.MediaStore;
 import android.util.Log;
 
 import java.util.ArrayList;
@@ -37,6 +36,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -65,6 +65,7 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
     private AudioOutputListener mListener;
     private final IAudioMixer<float[], short[]> mMixer;
     private ExecutorService mDecodeExecutorService;
+    private int sessionId = 0;
 
     public AudioOutput(AudioOutputListener listener) {
         mListener = listener;
@@ -80,7 +81,7 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
 
         int minBufferSize = AudioTrack.getMinBufferSize(AudioHandler.SAMPLE_RATE,
                 AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT);
-        mBufferSize = Math.min(minBufferSize, AudioHandler.FRAME_SIZE * 12);
+        mBufferSize = Math.min(minBufferSize, AudioHandler.FRAME_SIZE * 8);
         Log.v(Constants.TAG, "Using buffer size " + mBufferSize + ", system's min buffer size: " + minBufferSize);
 
         try {
@@ -95,21 +96,25 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
                 audioatrr.setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION);
                 audioatrr.setContentType(AudioAttributes.CONTENT_TYPE_SPEECH);
 
+                if (sessionId == 0) {
+                    sessionId = AudioManager.AUDIO_SESSION_ID_GENERATE;
+                }
+
                 mAudioTrack = new AudioTrack(
                         audioatrr.build(),
                         audiofmt.build(),
                         mBufferSize,
                         AudioTrack.MODE_STREAM,
-                        AudioManager.AUDIO_SESSION_ID_GENERATE
-                );
-                mAudioTrack.setVolume((float)0.99);
+                        sessionId);
+                mAudioTrack.setVolume((float)0.90);
             } else {
-                mAudioTrack = new AudioTrack(audioStream,
+                mAudioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL,
                         AudioHandler.SAMPLE_RATE,
                         AudioFormat.CHANNEL_OUT_MONO,
                         AudioFormat.ENCODING_PCM_16BIT,
                         mBufferSize,
-                        AudioTrack.MODE_STREAM);
+                        AudioTrack.MODE_STREAM,
+                        sessionId);
             }
         } catch (IllegalArgumentException e) {
             throw new AudioInitializationException(e);
@@ -153,33 +158,19 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
     @Override
     public void run() {
         Log.v(Constants.TAG, "Started audio output thread.");
-        android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
+        Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
         mRunning = true;
         mAudioTrack.play();
 
         final short[] mix = new short[mBufferSize];
 
         while(mRunning) {
-            if(fetchAudio(mix, 0, mBufferSize)) {
-                mAudioTrack.write(mix, 0, mBufferSize);
-                mAudioTrack.play();
-                PreprocessingEncoder.mEcho.echo_playback(mix);
-            } else {
-                synchronized (mInactiveLock) {
-                    mAudioTrack.play();
-                    mAudioTrack.pause();
-                    mAudioTrack.flush();
-                    mAudioTrack.stop();
-                    try {
-                        mInactiveLock.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                    mAudioTrack.play();
-                }
-            }
+            fetchAudio(mix, 0, mBufferSize);
+            mAudioTrack.write(mix, 0, mBufferSize);
+            PreprocessingEncoder.mEcho.echo_playback(mix);
         }
 
+        mAudioTrack.pause();
         mAudioTrack.flush();
         mAudioTrack.stop();
     }
@@ -196,10 +187,13 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
         Arrays.fill(buffer, bufferOffset, bufferOffset + bufferSize, (short) 0);
         final List<IAudioMixerSource<float[]>> sources = new ArrayList<>();
         try {
-            // Parallelize decoding using a fixed thread pool equal to the number of cores
+            if (!(mPacketLock.tryLock(20L, TimeUnit.MILLISECONDS))) {
+                return false;
+            }
+                // Parallelize decoding using a fixed thread pool equal to the number of cores
             List<Future<AudioOutputSpeech.Result>> futureResults =
                     mDecodeExecutorService.invokeAll(mAudioOutputs.values());
-            for(Future<AudioOutputSpeech.Result> future : futureResults) {
+            for (Future<AudioOutputSpeech.Result> future : futureResults) {
                 AudioOutputSpeech.Result result = future.get();
                 if (result.isAlive()) {
                     sources.add(result);
@@ -210,6 +204,7 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
                     speech.destroy();
                 }
             }
+            mPacketLock.unlock();
         } catch (InterruptedException e) {
             Log.v(Constants.TAG, "InterruptedException on FetchAudio " + e.getMessage());
             e.printStackTrace();
@@ -218,7 +213,6 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
             Log.v(Constants.TAG, "ExecutionException on FetchAudio " + e.getMessage());
             e.printStackTrace();
             return false;
-        } finally {
         }
 
         if (sources.size() == 0)
@@ -243,6 +237,7 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
 
             // Synchronize so we don't destroy an output while we add a buffer to it.
             try {
+                mPacketLock.lock();
                 AudioOutputSpeech aop = mAudioOutputs.get(session);
                 if(aop != null && aop.getCodec() != messageType) {
                     aop.destroy();
@@ -253,6 +248,7 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
                     try {
                         aop = new AudioOutputSpeech(user, messageType, mBufferSize, this);
                     } catch (NativeAudioException e) {
+                        mPacketLock.unlock();
                         Log.v(Constants.TAG, "Failed to create audio user "+user.getName());
                         e.printStackTrace();
                         return;
@@ -260,15 +256,20 @@ public class AudioOutput implements Runnable, AudioOutputSpeech.TalkStateListene
                     Log.v(Constants.TAG, "Created audio user "+user.getName());
                     mAudioOutputs.put(session, aop);
                 }
-
+                mPacketLock.unlock();
                 PacketBuffer dataBuffer = new PacketBuffer(pds.bufferBlock(pds.left()));
                 aop.addFrameToBuffer(dataBuffer, msgFlags, seq);
             } finally {
             }
+
             synchronized (mInactiveLock) {
                 mInactiveLock.notify();
             }
         }
+    }
+
+    public void setmAudioTrackSessionID(int sessId) {
+        sessionId = sessId;
     }
 
     @Override
